daf2f79cf07d49dd68f3e3c2acc59d846d1b531c
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-09-28 19:08:00 +0300

    navigation

---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+
+export const fetchMovies = async (query) => {
+  try {
+    //     const options = await axios.get() {
+    //   headers: {
+    //     Authorization:
+    //       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDE0NDU2YThiYjJjNmQ1M2IwMGFjNmYxZGIzNzFkYSIsIm5iZiI6MTcyNzUzNzU4Mi45NDE4NDgsInN1YiI6IjY2ZjQxMzc3Njg2MjhhMmVjMDBkZWQ3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H1gwKELYP0tKVs4al7-pzVY8e9SK10xAmOT3fjpeWPM",
+    //   },
+    // };
+  } catch {}
+};
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,18 @@
 import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
+import clsx from "clsx";
 export default function Navigation() {
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
   return (
     <div className={s.header}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
+      <NavLink className={buildLinkClass} to="/">
+        Home
+      </NavLink>
+      <NavLink className={buildLinkClass} to="/movies">
+        Movies
+      </NavLink>
     </div>
   );
 }
