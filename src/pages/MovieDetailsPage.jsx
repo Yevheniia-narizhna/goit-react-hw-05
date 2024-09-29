b57c9ec025c32193b78e0daa872ffadf60376a8b
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-09-29 23:14:41 +0300

    css

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,7 +1,7 @@
 import { Suspense, useEffect, useRef, useState } from "react";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 import { fetchTrendMoviesById } from "../components/Api/Api";
-
+import s from "./MovieDetailsPage.module.css";
 export default function MovieDetailsPage() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState();
@@ -28,36 +28,40 @@ export default function MovieDetailsPage() {
   return (
     <div>
       <Link to={goBackLink}>Go back</Link>
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
-          alt={movie.title}
-        />
-        <ul>
-          <li>
-            <h1>{movie.title}</h1>
-          </li>
-          <li>User score: {Math.round((movie.vote_average / 10) * 100)} %</li>
-          <li>Overview: {movie.overview}</li>
-          <li>
-            <h2>Genres:</h2>
-            {movie.genres.map((genre) => genre.name).join(", ")}
-          </li>
-        </ul>
-        <hr />
-        <h3>Additional information</h3>
-        <ul>
-          <li>
-            <Link to="cast">Cast</Link>
-          </li>
+      <div className={s.container}>
+        <div className={s.details}>
+          <img
+            src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <ul className={s.text}>
+            <li>
+              <h1>{movie.title}</h1>
+            </li>
+            <li>User score: {Math.round((movie.vote_average / 10) * 100)} %</li>
+            <li>Overview: {movie.overview}</li>
+            <li>
+              <h2>Genres:</h2>
+              {movie.genres.map((genre) => genre.name).join(", ")}
+            </li>
+          </ul>
+        </div>
+        <div className={s.info}>
+          <hr />
+          <h3>Additional information</h3>
+          <ul>
+            <li>
+              <Link to="cast">Cast</Link>
+            </li>
 
-          <li>
-            <Link to="reviews">Reviews</Link>
-          </li>
-        </ul>
-        <Suspense fallback={<div>Loading page...</div>}>
-          <Outlet context={{ movieId }} />
-        </Suspense>
+            <li>
+              <Link to="reviews">Reviews</Link>
+            </li>
+          </ul>
+          <Suspense fallback={<div>Loading page...</div>}>
+            <Outlet context={{ movieId }} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
